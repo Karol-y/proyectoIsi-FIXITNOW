3f88d4fe6307bb3c6eccda04836a1799c3fe2f2d
AlejandraRomero12354 <aromerob@unicartagena.edu.co>
2024-11-29 16:13:11 -0500

    Update menutrabajador.dart

--- FIXITNOW/lib/views/menutrabajador.dart
+++ FIXITNOW/lib/views/menutrabajador.dart
@@ -124,3 +124,121 @@ class MenuButton extends StatelessWidget {
   }
 }
 */
+import 'package:FIXITNOW/views/trabajadorperfil.dart';
+import 'package:flutter/material.dart';
+import 'package:FIXITNOW/main.dart'; // Importa la vista WorkerProfile
+
+void main() {
+  runApp(const MenuScreen());
+}
+
+class MenuScreen extends StatelessWidget {
+  const MenuScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Menú'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () {
+                // Acción al presionar el icono del menú
+                Navigator.pop(context); // Cerrar el diálogo
+              },
+            ),
+          ],
+        ),
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              // Botón "Perfil"
+              MenuButton(
+                text: 'PERFIL',
+                onPressed: () {
+                  // Navega a la vista WorkerProfile
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const WorkerProfile()),
+                  );
+                },
+              ),
+              const SizedBox(height: 10),
+
+              
+
+              // Botón "Calificaciones Obtenidas"
+              MenuButton(
+                text: 'CALIFICACIONES OBTENIDAS',
+                onPressed: () {
+                  // Implementa la acción correspondiente
+                },
+              ),
+              const SizedBox(height: 300),
+
+              // Botón "Salir"
+              SizedBox(
+                width: 400,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: const Color.fromARGB(255, 255, 255, 255),
+                    backgroundColor: const Color.fromARGB(255, 26, 95, 173),
+                    padding: const EdgeInsets.symmetric(vertical: 12.0),
+                    shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    side: const BorderSide(color: Color.fromARGB(255, 16, 74, 121)),
+                    ),
+                  ),
+                  onPressed: () {
+                    // Acción al presionar el botón de salir
+                    Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(builder: (context) => const ingresar()), // Redirige a la página de inicio de sesión
+                      (Route<dynamic> route) => false, // Elimina todas las rutas anteriores
+                    );
+                  },
+                  child: const Text('SALIR', style: TextStyle(fontWeight: FontWeight.bold)),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MenuButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed; // Callback para definir la acción del botón
+
+  // Constructor que acepta texto y una función de callback
+  const MenuButton({super.key, required this.text, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 220, // Ancho del botón centrado
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          foregroundColor: const Color.fromARGB(217, 255, 255, 255),
+          backgroundColor: const Color.fromARGB(255, 54, 147, 222),
+          padding: const EdgeInsets.symmetric(vertical: 16.0),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+            side: const BorderSide(color: Color.fromARGB(255, 54, 147, 222)),
+          ),
+        ),
+        onPressed: onPressed, // Ejecuta la acción del botón
+        child: Text(
+          text,
+          style: const TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
